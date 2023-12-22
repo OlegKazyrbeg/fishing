aa19a5a495d5fd518c66dec25e4db97a7f45dd88
OlegKazyrbeg <lazarev.906@mail.ru>
2023-12-23 00:20:46 +0300

    footer update

--- components/base/footer.jsx
+++ components/base/footer.jsx
@@ -4,8 +4,21 @@ const Footer = () => {
     return ( 
         <footer className="footer">
             <div className="container">
-                <div className="footer-content row">
-                    <p className="footer-text">footer</p>
+                <div className="footer-content">
+                    <div className="footer-content-top">
+                        <figure className='footer-logo'>
+                            <img src="/logo.png" alt="" />
+                        </figure>
+                        <div className="footer-text">Интернет-магазин морепродуктов "Аппетитная щучка"</div>
+                    </div>
+                    <nav>
+                        <ul className="footer-nav row">
+                            <li className="footer-nav-item">LINK</li>
+                            <li className="footer-nav-item">LINK</li>
+                            <li className="footer-nav-item">LINK</li>
+                            <li className="footer-nav-item">LINK</li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </footer>
