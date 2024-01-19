825714573e349dd842718001ebdde3a3cffb0fcb
OlegKazyrbeg <lazarev.906@mail.ru>
2024-01-20 00:04:13 +0300

    cardSlice recalculation

--- redux/slices/cardSlice.jsx
+++ redux/slices/cardSlice.jsx
@@ -5,6 +5,12 @@ const initialState = {
   items: []
 }
 
+const configurePrice = (state) => {
+  state.totalPrice = state.items.reduce((sum, obj) =>{
+    return (obj.price * obj.count) + sum;
+}, 0)
+}
+
 export const cartSlice = createSlice({
   name: 'card',
   initialState,
@@ -17,20 +23,23 @@ export const cartSlice = createSlice({
         } else {
             state.items.push({...action.payload, count: 1})
         }
-
-        state.totalPrice = state.items.reduce((sum, obj) =>{
-            return (obj.price * obj.count) + sum;
-        }, 0)
+        configurePrice(state)
     },
     minusItem(state, action) {
       const findItem = state.items.find(obj => obj.id === action.payload)
 
       if (findItem){
         findItem.count--
+        configurePrice(state)
+      }
+
+      if (findItem.count < 1){
+        state.items = state.items.filter(obj => obj.id !== action.payload)
       }
     },
     removeItem(state, action) {
         state.items = state.items.filter(obj => obj.id !== action.payload)
+        configurePrice(state)
     },
     clearItem(state) {
         state.items = []
