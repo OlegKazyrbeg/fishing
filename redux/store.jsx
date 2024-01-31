c0f961faaf7519576b6330dfa7832efa9a27c24d
OlegKazyrbeg <lazarev.906@mail.ru>
2024-01-31 16:46:35 +0300

    localStorage

--- redux/store.jsx
+++ redux/store.jsx
@@ -3,10 +3,19 @@ import filter from './slices/filterSlice'
 import card from './slices/cardSlice'
 import getCard from './slices/getSlice'
 
+const persistedState = localStorage.getItem('reduxState')
+  ? JSON.parse(localStorage.getItem('reduxState'))
+  : {};
+
 export const store = configureStore({
   reducer: {
     filter,
     card,
     getCard,
   },
+  preloadedState: persistedState
 })
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
